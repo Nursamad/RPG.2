ecab8af4622be2a35e2911ca64be48e0680fc47d
Nursamad <bagishbekow@gmail.com>
2021-06-05 18:51:54 +0600

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,17 @@ package com.company;
 import java.util.Random;
 
 public class Main {
-    public static int bossHealth = 1000;
+    public static int bossHealth = 5000;
     public static int bossDamage = 50;
     public static String bossDefenceType = "";
-    public static int[] heroesHealth = {300, 250, 95, 150};
-    public static int[] heroesDamage = {50, 45, 40, 0};
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medical"};
+    public static int[] heroesHealth = {300, 250, 200, 150, 500, 120, 180, 145};
+    public static int[] heroesDamage = {75, 55, 35, 0, 15, 10, 30, 40};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medical", "Tank", "Lovkach", "Berserk", "Tor"};
     public static int roundNumber = 0;
     public static int medicReability = 25;
+    public static Random random = new Random();
+    public static int tankTakeDamage = bossDamage / 5;
+    public static int toAllDamage = bossDamage - tankTakeDamage;
 
     public static void main(String[] args) {
         printStatistics();
@@ -23,17 +26,22 @@ public class Main {
         roundNumber++;
         System.out.println("Round №" + roundNumber);
         heroesHit();
+        lovkachDefence();
+        torStan();
         bossHits();
+        tankHelps();
         medicReabilytyHit();
-        printStatistics();
         changeDefence();
+        berserkBlock();
+        printStatistics();
     }
 
     public static void changeDefence() {
-        Random random = new Random();
         int randomIndex = random.nextInt(heroesAttackType.length);
         bossDefenceType = heroesAttackType[randomIndex];
-        System.out.println("Boss chose:" + bossDefenceType);
+        if (heroesHealth[randomIndex] > 0) {
+            System.out.println("Boss chose:" + bossDefenceType);
+        }
 
     }
 
@@ -60,37 +68,50 @@ public class Main {
         System.out.println("____________________________");
         System.out.println("Boss health " + bossHealth + " [ " + bossDamage + "]");
 
-        for (int i = 0; i < heroesHealth.length; i++)
+        for (int i = 0; i < heroesHealth.length; i++) {
             System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] + " [" + heroesDamage[i] + "]");
-        System.out.println("Medic give " + heroesHealth.length + " [" + medicReability + "]");
-
+        }
+        System.out.println();
     }
 
     public static void bossHits() {
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] - bossDamage <= 0) {
                 heroesHealth[i] = 0;
+            } else if (heroesHealth[4] > 0) {
+                heroesHealth[i] = heroesHealth[i] - toAllDamage;
             } else {
                 heroesHealth[i] = heroesHealth[i] - bossDamage;
-
             }
         }
 
     }
 
     public static void medicReabilytyHit() {
-        for (int i = 0; i < heroesHealth.length; i++) {
+        int randomHero = random.nextInt(heroesHealth.length);
+        if (heroesHealth[3] > 0 && heroesHealth[randomHero] > 0) {
+            if (heroesHealth[randomHero] - bossDamage <= 0) {
+                heroesHealth[randomHero] = 0;
+            } else {
+                heroesHealth[randomHero] = heroesHealth[randomHero] + medicReability;
+                System.out.println("\nMedic give: " + heroesAttackType[randomHero] + " [" + medicReability + "]");
 
-            if (heroesHealth[3] > 0 && heroesHealth[i] - bossDamage < heroesHealth[i]) {
-                if (heroesHealth[i] - bossDamage <= 0) {
-                    heroesHealth[i] = 0;
-                } else heroesHealth[i] = heroesHealth[i] + medicReability;
             }
         }
     }
 
+    public static void berserkBlock() {
+        int block = bossDamage / 2;
+        if (heroesHealth[6] - bossDamage <= 0) {
+            heroesHealth[6] = 0;
+        } else {
+            bossHealth = bossHealth - block;
+            System.out.println("Berserk damage: " + (heroesDamage[6] + block));
+        }
+    }
+
+
     public static void heroesHit() {
-        Random random = new Random();
         int coeff = random.nextInt(5);
         for (int i = 0; i < heroesDamage.length; i++) {
             if (bossHealth > 0 && heroesHealth[i] > 0) {
@@ -112,5 +133,48 @@ public class Main {
 
         }
     }
+
+    public static void lovkachDefence() {
+        boolean chance = random.nextBoolean();
+        if (heroesHealth[5] <= 0) {
+            heroesHealth[5] = 0;
+        } else {
+            if (chance) {
+                heroesHealth[5] += bossDamage;
+                System.out.println("Lucky dodged!");
+            }
+        }
+    }
+
+    public static void tankHelps() {
+        if (heroesHealth[4] <= 0) {
+            heroesHealth[4] = 0;
+        }
+        /*  while (heroesHealth[4] > 0 ) {
+            for (int i = 0; i < heroesHealth.length; i++) {
+
+                heroesHealth[i] += tankTakeDamage;
+            }
+            heroesHealth[4] -= tankTakeDamage * heroesHealth.length;
+        }
+*/
+        else {
+            heroesHealth[4] -= tankTakeDamage * heroesHealth.length;
+            System.out.println("Tank take: " + (tankTakeDamage * (heroesHealth.length - 1)));
+        }
+
+
+    }
+
+    public static void torStan() {
+        int stun = random.nextInt(4);
+        if (heroesHealth[7] <= 0) {
+            heroesHealth[7] = 0;
+        } else if (stun == 1) {
+            for (int i = 0; i < heroesHealth.length; i++) {
+                heroesHealth[i] +=  toAllDamage;
+            }
+            System.out.println("Tor stan boss: " + stun + " round");
+        }
+    }
 }
-// проблемы : нанесение урона после смерти
